bb56f761b29bdbc245bee8696186732b1cc8aa2e
drss.ftc.12653 <drss.ftc.12653@gmail.com>
2021-10-23 11:41:10 -0400

    Fixed a bunch of tele-op code
    -AG

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ProgrammingBoardTeleOp.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+import static android.os.SystemClock.sleep;
+
+@TeleOp(name = "programming board TeleOp", group = "default")
+public class ProgrammingBoardTeleOp extends LinearOpMode {
+
+    //define motors and stuff
+    DcMotor Large;
+    DcMotor Small;
+    DcMotor Rev;
+    Servo Pass;
+    ColorSensor RubberDuck;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        //hardware maps
+        Large = hardwareMap.dcMotor.get("LM"); //port 3
+        Small = hardwareMap.dcMotor.get("SM"); //port 0
+        Rev = hardwareMap.dcMotor.get("HM"); //port 2
+        Pass = hardwareMap.servo.get("S"); // medium port 3
+        RubberDuck = hardwareMap.colorSensor.get("RD"); //small port 0
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            if (gamepad1.dpad_up) {
+                Large.setPower(0.5);
+                sleep(1000);
+                Large.setPower(0);
+            }
+        }
+
+    }
+}
