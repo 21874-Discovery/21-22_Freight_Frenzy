b0753397c6c691a494911dd1d80acaa7720faa33
drss.ftc.12653 <drss.ftc.12653@gmail.com>
2021-11-23 17:36:16 -0500

    Added Mechnum to TELE0p-LA

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FY21Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FY21Autonomous.java
@@ -2,13 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.DcMotor.RunMode;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.ColorSensor;
-import com.qualcomm.robotcore.util.Range;
 
 
 @Autonomous(name = "FY21Auto", group = "team")
@@ -106,4 +100,20 @@ public void DCSUPERCOLOR (){
         }
     }
 }
+    public void Mechnum_drive () {
+
+
+        double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
+        double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+        double rightX = gamepad1.right_stick_x;
+        final double v1 = r * Math.cos(robotAngle) + rightX;
+        final double v2 = r * Math.sin(robotAngle) - rightX;
+        final double v3 = r * Math.sin(robotAngle) + rightX;
+        final double v4 = r * Math.cos(robotAngle) - rightX;
+
+        topRight.setPower(v1);
+        bottomRight.setPower(v2);
+        topLeft.setPower(v3);
+        bottomLeft.setPower(v4);
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FY21Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FY21Teleop.java
@@ -6,9 +6,47 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name = "FY21 TeleOp", group = "TeleOp")
 public class FY21Teleop extends LinearOpMode {
-    DcMotor Large;
+    DcMotor topRight;
+    DcMotor bottomRight;
+    DcMotor topLeft;
+    DcMotor bottomLeft;
+
     @Override
     public void runOpMode() throws InterruptedException {
         //Engineers using REV CoreHex motors for "spinney box" and Linear Slide
+        //hardware maps
+        topRight = hardwareMap.dcMotor.get("TR"); //port 0
+        bottomRight = hardwareMap.dcMotor.get("BR"); //port 1
+        topLeft = hardwareMap.dcMotor.get("TL"); //port 2
+        bottomLeft = hardwareMap.dcMotor.get("BL"); //port 3
+        waitForStart();
+        while (opModeIsActive()) {
+            if (gamepad1.x) {
+
+            }
+            if (gamepad1.y) {
+
+            }
+            if (gamepad1.b) {
+
+            }
+            if (gamepad1.a) {
+
+            }
+
+
+            double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
+            double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+            double rightX = gamepad1.right_stick_x;
+            final double v1 = r * Math.cos(robotAngle) + rightX;
+            final double v2 = r * Math.sin(robotAngle) - rightX;
+            final double v3 = r * Math.sin(robotAngle) + rightX;
+            final double v4 = r * Math.cos(robotAngle) - rightX;
+
+            topRight.setPower(v1);
+            bottomRight.setPower(v2);
+            topLeft.setPower(v3);
+            bottomLeft.setPower(v4);
+        }
     }
 }
