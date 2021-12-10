a2ef60a9c8f1e1592a4113d9470f37c6ce8d5f60
drss.ftc.12653 <drss.ftc.12653@gmail.com>
2021-12-10 16:19:51 -0500

    fixed a thing so autonomous doesnt complain
    -GB

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FY21BlueCaresell.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FY21BlueCaresell.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 
-@Autonomous(name = "FY21Auto", group = "team")
+@Autonomous(name = "FY21AutoBlue", group = "team")
 
 public class FY21BlueCaresell extends LinearOpMode {
    //define motors and stuff
