67b24b09c1491affcef853f9b2f538df0b8e488d
drss.ftc.12653 <drss.ftc.12653@gmail.com>
2022-01-21 17:11:29 -0500

    Testing the mechneum drive and turn
    -DH

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FY21RedAutoStorege.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FY21RedAutoStorege.java
@@ -64,9 +64,12 @@ public class FY21RedAutoStorege extends LinearOpMode {
                 telemetry.addData("inside currentstep:", currentstep);
                 telemetry.update();
                 //Move Forward 0.5
-
+                Mecanum_drive("Backward", 0.5, 525);
+                carouselSpinner.setPower(1);
+                sleep(1000);
+                carouselSpinner.setPower(0);
+                Mecanum_Turn("Left", 1, 400);
                 Mecanum_drive("Forward", 0.5, 1000);
-                Mecanum_Turn("Left", 1, 250);
                 currentstep++;
             }
         }
